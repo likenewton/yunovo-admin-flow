00936ea5159f15de4a614830a17e5246fbc2e552
chenjizhong <chenjizhong@yunovo.cn>
2019-06-10 14:28:56 +0800

    add

---
+++ fc/model/src/main/java/cn/yunovo/iov/fc/model/form/OrgForm.java
@@ -0,0 +1,41 @@
+package cn.yunovo.iov.fc.model.form;
+
+import java.io.Serializable;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+@ApiModel
+public class OrgForm implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@ApiModelProperty("机构名称")
+	private String name; 
+	
+	@ApiModelProperty("所属父机构id")
+	private Integer parent_id;
+	
+	@ApiModelProperty("可开账户数量")
+	private Integer user_total;
+	
+	@ApiModelProperty("异步通知地址")
+	private String notify_url;
+	
+	@ApiModelProperty("机构描述")
+	private String memo; 
+	
+	@ApiModelProperty("负责人邮箱")
+	private String email;
+	
+	@ApiModelProperty("负责人手机")
+	private String tel;
+	
+	@ApiModelProperty("返利比率")
+	private Double rebate_value;
+}
