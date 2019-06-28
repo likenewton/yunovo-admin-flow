61b88f6db8d26a0a7af452725a5d5e87861b7209
chenjizhong <chenjizhong@yunovo.cn>
2019-06-28 18:11:00 +0800

    add

--- fc/fc-web/src/main/java/cn/yunovo/iov/fc/web/controller/gprs/GprsCardResetController.java
+++ fc/fc-web/src/main/java/cn/yunovo/iov/fc/web/controller/gprs/GprsCardResetController.java
@@ -49,7 +49,7 @@ public class GprsCardResetController extends BaseController{
 	@ApiOperation(value="业务管理-流量卡重置接口")
 	@RequestMapping(path="/gprs",method= {RequestMethod.POST})
 	public Result<List<CardRestBean>> cardReset(@RequestBody CardResetForm form) {
-		form.validate(null);
+		form.validate();
 		List<CardRestBean>  data = iCcResetLogService.cardReset(form, this.getLoginBaseInfo());
 		return ResultUtil.success(data);
 	}
