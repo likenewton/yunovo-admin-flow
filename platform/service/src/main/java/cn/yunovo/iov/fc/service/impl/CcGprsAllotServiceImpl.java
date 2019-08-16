d0697fd6991363ec5b76587cc657ebeae577ec2c
chenjizhong <chenjizhong@yunovo.cn>
2019-08-16 18:45:53 +0800

    APK白名单流量消耗上报，单张卡多次上报后报错

--- platform/service/src/main/java/cn/yunovo/iov/fc/service/impl/CcGprsAllotServiceImpl.java
+++ platform/service/src/main/java/cn/yunovo/iov/fc/service/impl/CcGprsAllotServiceImpl.java
@@ -496,7 +496,7 @@ public class CcGprsAllotServiceImpl extends ServiceImpl<ICcGprsAllotMapper, CcGp
 			}
 		}*/
 		
-		if(data.getUnicom_stop() == 1 && data.getMax_unused() > 0 && (gprs.getOpen_card() == null || !gprs.getOpen_card())) {
+		if(data.getUnicom_stop() == 1 && data.getMax_unused() > 0 && gprs.getOpen_card() != null && gprs.getOpen_card()) {
 			
 			String username = (data.getCard_type() == 1 && DateUtil.getDayOfMonth(null) == 1) || (data.getCard_type() >= 2 && DateUtil.getDayOfMonth(null) == 27) ? "mend" : "topup";
 			if(this.cardOnoff(card, 0, 0, username)) {
