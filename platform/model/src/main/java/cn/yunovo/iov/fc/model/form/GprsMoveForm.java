417d44b540c254c7b7557ce39cf439639d881226
chenjizhong <chenjizhong@yunovo.cn>
2019-08-19 09:36:59 +0800

    表单验证

--- platform/model/src/main/java/cn/yunovo/iov/fc/model/form/GprsMoveForm.java
+++ platform/model/src/main/java/cn/yunovo/iov/fc/model/form/GprsMoveForm.java
@@ -3,6 +3,7 @@ package cn.yunovo.iov.fc.model.form;
 import java.io.Serializable;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 import cn.yunovo.iov.fc.model.BaseForm;
 import io.swagger.annotations.ApiModel;
@@ -20,14 +21,17 @@ public class GprsMoveForm extends BaseForm implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@Size(max=20, message="旧卡ICCID长度不能超过20个字符")
 	@NotEmpty(message="请输入正确的旧卡ICCID")
 	@ApiModelProperty("旧卡ICCID")
 	private String old_card_iccid;
 	
+	@Size(max=20, message="新卡ICCID长度不能超过20个字符")
 	@NotEmpty(message="请输入正确的新卡ICCID")
 	@ApiModelProperty("新卡ICCID")
 	private String new_card_iccid;
 	
+	@Size(max=255, message="迁移备注内容不能超过255个字符")
 	@ApiModelProperty("迁移备注")
 	private String move_memo;
 }
